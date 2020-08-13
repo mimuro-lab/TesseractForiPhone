f291779f6b3e424c0bc62b5ad07dda674bc12e2a
mimuro-lab <mimurosyunya@gmail.com>
2020-08-13 16:11:38 +0900

    画像プレビューの為のswiftファイルの追加

---
+++ TesseractForiPhone/PreviewImage.swift
@@ -0,0 +1,30 @@
+//
+//  PreviewImage.swift
+//  TesseractForiPhone
+//
+//  Created by プログラム開発用パソコン02 on 2020/08/13.
+//  Copyright © 2020 wings. All rights reserved.
+//
+
+import UIKit
+
+class PreviewImage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
