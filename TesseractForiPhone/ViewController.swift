558d47dd499c0d7378b9962387ffbda5197ba45f
mimuro-lab <mimurosyunya@gmail.com>
2020-08-13 15:34:34 +0900

    プロジェクトファイルの追加

---
+++ TesseractForiPhone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TesseractForiPhone
+//
+//  Created by プログラム開発用パソコン02 on 2020/08/13.
+//  Copyright © 2020 wings. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
